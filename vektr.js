d57703e6d4b2486a595d9065d35b8da10558fd6a
kslicker <90402590+kslicker@users.noreply.github.com>
2023-04-07 22:42:23 +0100

    .

--- vektr.js
+++ vektr.js
@@ -1,11 +1,11 @@
+// Mouse over tooltip
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
 });
 
-// Create a Fabric wrapper around native canvas element and query selector for canvas dimensions
-var canvas = document.querySelector('canvas');
-var canvas = new fabric.Canvas('canvas', {
-});
+// Create a Fabric wrapper around native canvas element
+var canvas = new fabric.Canvas('canvas');
+console.log(canvas);
 
 var circle = new fabric.Circle({
     radius: 20, fill: 'green', left: 100, top: 100
@@ -13,5 +13,9 @@ var circle = new fabric.Circle({
   var triangle = new fabric.Triangle({
     width: 20, height: 30, fill: 'blue', left: 50, top: 50
   });
+var line = new fabric.Line((20,20,50,50),{
+    strokeWidth: 4, stroke: 'black'
+  });
   
-  canvas.add(circle, triangle);
+canvas.add(circle, triangle, line);
+console.log(triangle);
