747d31e699fc8ee493b5b92c40cb90cc9e01719d
kslicker <90402590+kslicker@users.noreply.github.com>
2023-04-08 16:47:22 +0100

    container

--- vektr.js
+++ vektr.js
@@ -19,9 +19,9 @@ var line = new fabric.Line((20,20,50,50),{
 canvas.add(circle, triangle, line);
 
 // Update Fabric canvas size
-   window.setInterval(() => {
-    canvas.setDimensions({
-        width: document.getElementById('canvas-div').clientWidth,
-        height: document.getElementById('canvas-div').clientHeight
-    }, 1000);
-});
+//   window.setInterval(() => {
+//    canvas.setDimensions({
+//        width: document.getElementById('canvas-div').clientWidth - 70,
+//        height: document.getElementById('canvas-div').clientHeight
+//    }, 1000);
+//});
