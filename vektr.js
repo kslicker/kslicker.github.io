18211f75daa1191f09a133fd55bc143d3de9b79b
kslicker <90402590+kslicker@users.noreply.github.com>
2023-04-14 23:35:41 +0100

    .

--- vektr.js
+++ vektr.js
@@ -5,20 +5,27 @@ $(document).ready(function(){
 
 // Variables
 var stroke_color = 'black';
+var counter = 0;
 
 // Create a Fabric wrapper around native canvas element
 var canvas = new fabric.Canvas('canvas', {
     backgroundColor:'white'
 });
 
+// Add shape for testing purposes 
 var circle1 = new fabric.Circle({
     radius: 20, fill: 'green', left: 100, top: 100
   });
-  var triangle = new fabric.Triangle({
+
+var circle2 = new fabric.Circle({
+radius: 20, fill: 'blue', left: 200, top: 200
+});
+
+var tri = new fabric.Triangle({
     width: 20, height: 30, fill: 'blue', left: 50, top: 50
-  });
-  
-  canvas.add(circle1, triangle);
+});
+
+canvas.add(circle1, circle2, tri);
 
 // Update Fabric canvas size to match browser window
 //window.setInterval(() => {
@@ -35,7 +42,7 @@ zoom.addEventListener("input", function() {
     canvas.setZoom(zoom.value);
 }) 
 
-// Event Listeners //
+//// Event Listeners ////
 
 // Erase Canvas
 document.getElementById('erase').addEventListener('click', function(){
@@ -58,16 +65,21 @@ document.getElementById('draw').addEventListener('click', function(){
 
 // Circle
 document.getElementById('circle').addEventListener('click', function(){
+
+    // Check if there is already a circle at this location to avoid overlapping
+    //c_objects = canvas.toObject();
+    //for (let i = 0; i < c_objects.objects.length; i++) {
+    //    if (c_objects.objects[i]['type'] == 'circle' && c_objects.objects[i]['top'] == 100 && c_objects.objects[i]['left'] == 100) {
+    //        console.log('gotcha!');
+    //    }
+    //}
+
+    // Counters to shift circle positioning to avoid overlapping
+
     circle = new fabric.Circle({
-        radius: 50, stroke: stroke_color, left: 100, top: 100 
+        radius: 50, stroke: stroke_color, fill: 'transparent', left: 100 + counter, top: 100 + counter
     })
     canvas.add(circle);
     canvas.setActiveObject(circle);
+    counter += 10;
 })
-
-// Function to get all objects on canvas
-function serializeCanvas(c) {
-    var canvasJson = JSON.stringify(c);
-    console.log(canvasJson);
-}
- serializeCanvas(canvas);
